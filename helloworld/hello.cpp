c70425f444c4072f4c84fb2222b15611ae0dd715
Benjamín Buske <info@buske-it.com>
2019-01-22 19:35:09 -0400

    Create hello.cpp
    First commit. Simple hello.cpp file in C++, that prints the famous "Hello World" to the console.

---
+++ helloworld/hello.cpp
@@ -0,0 +1,9 @@
+// Simple Script that prints the famous phrase "Hello World!" to the console.  
+
+#include <iostream>                 // Include the standard input-/output stream library in this file.
+
+int main() {                        // The main function marks the beginning of the actual code / program.
+
+  std::cout << "Hello World!\n";    // Tell computer to print hello world to the console. The \n marks a new line. 
+  return: 0;                        // If no errors are encountered, the program returns 0. Can be omitted. 
+}
