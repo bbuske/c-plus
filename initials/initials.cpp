05b2c1bbebeac74a468b9b2d6907c00f0657ba38
Benjamín Buske <info@buske-it.com>
2019-01-22 20:08:25 -0400

    Create initials.cpp
    A basic program written in C++ to print your initials.

---
+++ initials/initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>                   // Load the C++ standard input / output stream
+
+int main() {                          // Our program starts here
+
+std::cout << "BBBB    BBBB\n";
+std::cout << "B   B   B   B\n";
+std::cout << "B   B   B   B\n";
+std::cout << "BBBB    BBBB\n";
+std::cout << "B   B   B   B\n";
+std::cout << "B   B   B   B\n";
+std::cout << "BBBB    BBBB\n";
+
+  return 0;                            // If the program runs without errors, 0 will be returned. Can be omitted. 
+}
